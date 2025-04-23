99c54677d066cbf90190e03c5fac3678443b1143
sinnerdev <marcomarraro@gmail.com>
2025-04-23 16:22:17 +0200

    Add button for manage status of btn hover/click

--- script.js
+++ script.js
@@ -2,15 +2,45 @@ console.log('Im working');
 
 // manipolazione del container principale
 const container = document.querySelector('#grid-container');
+const toggleBtn = document.querySelector('.toggle-btn');
+let toggleState = true;
+let toggleBtnText = document.querySelector('.hover-click')
 
-//creazione della griglia con cicli annidati
-//primo ciclo crea le 16 row
-//secondo ciclo crea le 16 colonne per ogni row
-for(let i = 1 ; i<=16 ; i++){
+
+// handler function
+function targetElementHover(e) {
+    if (e.target.classList.contains('div-column')) {
+        e.target.style.backgroundColor = 'black';
+    }
+}
+
+function targetElementClick(e) {
+    if (e.target.classList.contains('div-column')) {
+        e.target.style.backgroundColor = 'orange';
+    }
+}
+
+function typeInteraction() {
+    container.removeEventListener('mouseover', targetElementHover);
+    container.removeEventListener('click', targetElementClick);
+
+    if (toggleState) {
+
+        container.addEventListener('mouseover', targetElementHover);
+        toggleBtnText.textContent = 'HOVER';
+
+    }
+    else {
+        container.addEventListener('click', targetElementClick);
+        toggleBtnText.textContent = 'CLICK';
+    }
+}
+
+for (let i = 1; i <= 16; i++) {
     let rowDiv = document.createElement('div');
     rowDiv.classList.add('div-row');
-    
-    for (let j = 1; j<=16; j++){
+
+    for (let j = 1; j <= 16; j++) {
         let columnDiv = document.createElement('div');
         columnDiv.classList.add('div-column');
         rowDiv.appendChild(columnDiv);
@@ -18,14 +48,28 @@ for(let i = 1 ; i<=16 ; i++){
     container.appendChild(rowDiv);
 }
 
+typeInteraction();
+
+toggleBtn.addEventListener('click', function() {
+    toggleState = !toggleState; 
+    typeInteraction(); 
+});
+
+
+
+
+
+
+
+
+
+
+//creazione della griglia con cicli annidati
+//primo ciclo crea le 16 row
+//secondo ciclo crea le 16 colonne per ogni row
+
 
 //evento per gestire hover su elemento container (event delegation)
-
-container.addEventListener('mouseover', (e)=>{
-    if (e.target.classList.contains('div-column')){
- e.target.style.backgroundColor = 'black';}
-    
-})
 
 
 
