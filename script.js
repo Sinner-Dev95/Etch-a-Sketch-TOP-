6b3aec0589d1778baec0f2d3f50594b3ac793ba5
sinnerdev <marcomarraro@gmail.com>
2025-04-23 17:01:22 +0200

    fixed grid js and resolve exercise

--- script.js
+++ script.js
@@ -20,6 +20,23 @@ function targetElementClick(e) {
     }
 }
 
+//FUNCTION FOR GRID SIZE
+function gridDimension(size) {
+    for (let i = 1; i <= size; i++) {
+        let rowDiv = document.createElement('div');
+        rowDiv.classList.add('div-row');
+
+        for (let j = 1; j <= size; j++) {
+            let columnDiv = document.createElement('div');
+            columnDiv.classList.add('div-column');
+            rowDiv.appendChild(columnDiv);
+        }
+        container.appendChild(rowDiv);
+    }
+};
+
+gridDimension(16);
+
 function typeInteraction() {
     container.removeEventListener('mouseover', targetElementHover);
     container.removeEventListener('click', targetElementClick);
@@ -34,27 +51,41 @@ function typeInteraction() {
         container.addEventListener('click', targetElementClick);
         toggleBtnText.textContent = 'CLICK';
     }
+
 }
 
-for (let i = 1; i <= 16; i++) {
-    let rowDiv = document.createElement('div');
-    rowDiv.classList.add('div-row');
 
-    for (let j = 1; j <= 16; j++) {
-        let columnDiv = document.createElement('div');
-        columnDiv.classList.add('div-column');
-        rowDiv.appendChild(columnDiv);
-    }
-    container.appendChild(rowDiv);
-}
 
 typeInteraction();
 
-toggleBtn.addEventListener('click', function() {
-    toggleState = !toggleState; 
-    typeInteraction(); 
+toggleBtn.addEventListener('click', function () {
+    toggleState = !toggleState;
+    typeInteraction();
 });
 
+function removeGrid() {
+    while (container.firstChild) {
+        container.removeChild(container.firstChild);
+    }
+}
+
+const sizeBtn = document.querySelector('.size-btn');
+
+sizeBtn.addEventListener('click', function(){
+    let input = prompt('Inserisci il numero di celle (limite masso 100)')
+
+    let newInput= parseInt(input)
+
+    if (isNaN(newInput) || newInput <= 0 || newInput > 100){
+        alert('Per favore inserisci un numero valido tra 1 e 100');
+        return; // Esce dalla funzione se l'input non è valido
+    }
+    
+    // Se arriviamo qui, l'input è valido
+    removeGrid(); 
+    gridDimension(newInput);
+    typeInteraction(); 
+})
 
 
 
@@ -62,14 +93,6 @@ toggleBtn.addEventListener('click', function() {
 
 
 
-
-
-//creazione della griglia con cicli annidati
-//primo ciclo crea le 16 row
-//secondo ciclo crea le 16 colonne per ogni row
-
-
-//evento per gestire hover su elemento container (event delegation)
 
 
 
