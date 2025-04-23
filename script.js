0621ccc018eb57cdaa4fd7c9c21d89fa43c9fbd2
sinnerdev <marcomarraro@gmail.com>
2025-04-23 11:40:11 +0200

    Develop the logic for grid in flexbox

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
 console.log('Im working');
+
+
+const container = document.querySelector('#grid-container');
+
+for(let i = 1 ; i<=16 ; i++){
+    let rowDiv = document.createElement('div');
+    rowDiv.classList.add('div-row');
+    
+    for (let j = 1; j<=16; j++){
+        let columnDiv = document.createElement('div');
+        columnDiv.classList.add('div-column');
+        rowDiv.appendChild(columnDiv);
+    }
+    container.appendChild(rowDiv);
+}
